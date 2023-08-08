167e4edaef2ac0593a95f0ca3f64b5d837dcdb25
Shriraman-Hegde <kovesarraman@gmail.com>
2023-08-08 13:37:31 +0530

    Styling with angular material (#5)
    * Replacing bootstrap with angularmaterial
    
    * Updating unit test cases
    
    * Updating cypress e2e test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let app:AppComponent;
+  let fixture:ComponentFixture<AppComponent>;
+  let dom;
+  let button;
+
+  beforeEach(async () => {await TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      MatListModule,
+      MatToolbarModule
+    ],
+    declarations: [
+      AppComponent
+    ],
+  }).compileComponents();
+});
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.autoDetectChanges(true);
+    app = fixture.debugElement.componentInstance;
+    dom = fixture.debugElement.nativeElement;
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ng-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('ng-demo');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('nav')?.textContent).toContain('Welcome to ng-demo!');
+    expect(compiled.querySelector('mat-toolbar')?.textContent).toContain('Welcome to ng-demo!');
   });
 });
